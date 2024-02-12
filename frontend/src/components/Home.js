f3a754a2471edb6b48c8f31e8b2b18513cae2b9f
sorasak menphurk <sorasak_456123@hotmail.com>
2024-02-12 20:05:32 +0700

    update home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -13,8 +13,8 @@ const Home = () => {
                         </a>
                         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                             <li><a href="#" className="nav-link px-2 link-secondary text-light">|</a></li>
-                            <li><a href="contact.html" className="nav-link px-2 link-body-emphasis text-light">Contact Us</a></li>
-                            <li><a href="about.html" className="nav-link px-2 link-body-emphasis text-light">About Us</a></li>
+                            <li><a href="/contact" className="nav-link px-2 link-body-emphasis text-light">Contact Us</a></li>
+                            <li><a href="/about" className="nav-link px-2 link-body-emphasis text-light">About Us</a></li>
                         </ul>
                         <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
                             <input type="search" className="form-control" placeholder="Search..." aria-label="Search" />
